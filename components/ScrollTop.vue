d35691222fb3a99c828947cf423731a52bee58f0
Adewumi Adedeji <adewumiadedeji27@gmail.com>
2021-07-03 23:54:40 +0100

    first release

--- components/ScrollTop.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <client-only>
-        <back-to-top class="scroll-top" bottom="30px">
-            <i class="icofont-arrow-up"></i>
-        </back-to-top>
-    </client-only>
-</template>
