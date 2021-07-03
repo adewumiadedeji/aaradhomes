4117b4d8a42d6365538d0cfcd55489d043e698c9
Adewumi Adedeji <adewumiadedeji27@gmail.com>
2021-07-03 22:45:46 +0100

    First version for dev

---
+++ components/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="hero-section">
+        <img class="shape shape1" src="/images/hero/shape1.png" alt="404" />
+        <img class="shape shape2" src="/images/hero/shape2.png" alt="404" />
+        <img class="shape particle1" src="/images/hero/particle1.png" alt="404" />
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-xl-6">
+                    <div class="hero-content">
+                        <h2 class="title">Radiography.<br> Nuclear Medicine.<br> Diagnostic Imaging.</h2>
+                        
+                        <img class="particle3" src="/images/gallery/particle3.png" alt="particle2" />
+                    </div>
+                </div>
+                <div class="col-lg-5 col-xl-6">
+                    <div class="hero-img">
+                        <img class="animate-one" src="/images/gallery/1.png" alt="404" data-aos="zoom-in" data-aos-delay="100" />
+                        <div class="position-absolute animate-two">
+                            <img data-aos="fade-up" data-aos-delay="600" src="/images/gallery/2.png" alt="404" />
+                        </div>
+                        <div class="position-absolute animate-three">
+                            <img data-aos="fade-down" data-aos-delay="400" src="/images/gallery/3.png" alt="404" />
+                        </div>
+                    </div>
+                    <div class="hero-img-mobile">
+                        <img src="/images/hero/mobile.png" alt="images-not_found" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
